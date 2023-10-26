29d705a457f076e62342648e6ec4bc5027203cbe
nishanthan <nishanthan@codevus.com>
2023-10-26 21:37:17 +0530

    context api

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import userContext from "../contexts/userContext";
 
 function Dashboard() {
-    return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
-    );
-  }
-  
-  export default Dashboard;
-  
+  const value = useContext(userContext);
+  const [user, setUser] = value;
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <pre>{JSON.stringify(user)}</pre>
+      <button
+        onClick={() => {
+          setUser({});
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Dashboard;
