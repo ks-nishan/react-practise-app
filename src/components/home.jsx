9d085845d3c2851deca6d04c85528efdf57bd696
nishanthan <nishanthan@codevus.com>
2024-01-30 20:11:23 +0530

    conditional render using ternary operator

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <h1>Home</h1>
+      {/* <h2>Welcome Nishanthan!</h2>
+      <h2>Welcome Guest!!</h2> */}
+      {isLoggedIn ? <h2>Welcome Nishanthan!</h2> : <h2>Welcome Guest!!</h2>}
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Login</button>
     </div>
   );
 }
