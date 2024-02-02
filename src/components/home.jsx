e6d32920f780fc55904e5e20ab850ddb68f0ebe4
nishanthan <nishanthan@codevus.com>
2024-02-02 07:27:23 +0530

    use ref basic usecase

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 function Home() {
+  const [name, setName] = useState("");
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+
   return (
     <div>
-      <h1>Home</h1>
+      <h2>Home</h2>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <p>My name is {name}</p>
+      <p>Component rendered {renderCount.current} times</p>
     </div>
   );
 }
