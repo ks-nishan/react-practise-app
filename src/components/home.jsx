fafb29914e201167ca767a5b81c71286751d0bc3
nishanthan <nishanthan@codevus.com>
2024-02-02 07:40:07 +0530

    referencing the html elements

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,17 +3,23 @@ import React, { useEffect, useRef, useState } from "react";
 function Home() {
   const [name, setName] = useState("");
   const renderCount = useRef(1);
+  const inputRef = useRef();
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
 
+  const focus = () =>{
+    inputRef.current.focus();
+  }
+
   return (
     <div>
       <h2>Home</h2>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
       <p>My name is {name}</p>
       <p>Component rendered {renderCount.current} times</p>
+      <button onClick={focus}>Focus</button>
     </div>
   );
 }
