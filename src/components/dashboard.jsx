247450fa24b2c4b9fc3f78e9b6f2287c863a1e25
nishanthan <nishanthan@codevus.com>
2024-02-02 08:02:41 +0530

    previours state value tracking

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Dashboard() {
+  const [name, setName] = useState('');
+  const prevName = useRef('');
+
+  useEffect(() => {
+    prevName.current = name
+  }, [name]);
+
+
     return (
       <div>
         <h1>Dashboard</h1>
+        <input value={name} onChange={(e) => setName(e.target.value)}/>
+        <p>My name is <span style={{color: 'red'}}>{name}</span> and my previous name is <span style={{color: 'blue'}}>{prevName.current}</span></p>
       </div>
     );
-  }
+  } 
   
   export default Dashboard;
   
