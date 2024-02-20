1394879cb1571017f74867390b53f9101aae42d2
nishanthan <nishanthan@codevus.com>
2024-02-20 08:22:01 +0530

    feature: basic useMemo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useMemo, useState } from "react";
 
 function Home() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
+  function slowFunction(num) {
+    console.log("Calling slow function");
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num * 2;
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyle}>{doubleNumber}</div>
     </div>
   );
 }
